ad8def89b4ef442ffe553f43ff2e63a9f56f89b1
silent-cipher <puspendramahariya@gmail.com>
2025-10-30 17:47:26 +0530

    feat: add failed retrievals tracking and analysis endpoints

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -3,16 +3,19 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { DatabaseModule } from "../database/database.module.js";
 import { Deal } from "../database/entities/deal.entity.js";
 import { MetricsDaily } from "../database/entities/metrics-daily.entity.js";
+import { Retrieval } from "../database/entities/retrieval.entity.js";
 import { SpPerformanceAllTime } from "../database/entities/sp-performance-all-time.entity.js";
 import { SpPerformanceLastWeek } from "../database/entities/sp-performance-last-week.entity.js";
 import { StorageProvider } from "../database/entities/storage-provider.entity.js";
 import { DailyMetricsController } from "./controllers/daily-metrics.controller.js";
 import { FailedDealsController } from "./controllers/failed-deals.controller.js";
+import { FailedRetrievalsController } from "./controllers/failed-retrievals.controller.js";
 import { NetworkStatsController } from "./controllers/network-stats.controller.js";
 
 import { ProvidersController } from "./controllers/providers.controller.js";
 import { DailyMetricsService } from "./services/daily-metrics.service.js";
 import { FailedDealsService } from "./services/failed-deals.service.js";
+import { FailedRetrievalsService } from "./services/failed-retrievals.service.js";
 import { MetricsSchedulerService } from "./services/metrics-scheduler.service.js";
 import { NetworkStatsService } from "./services/network-stats.service.js";
 import { ProvidersService } from "./services/providers.service.js";
@@ -24,6 +27,7 @@ import { ProvidersService } from "./services/providers.service.js";
  * - Provider performance tracking (materialized views)
  * - Daily time-series metrics
  * - Failed deals analysis
+ * - Failed retrievals analysis
  * - Network-wide statistics
  *
  * Architecture:
@@ -34,10 +38,37 @@ import { ProvidersService } from "./services/providers.service.js";
 @Module({
   imports: [
     DatabaseModule,
-    TypeOrmModule.forFeature([SpPerformanceLastWeek, SpPerformanceAllTime, MetricsDaily, Deal, StorageProvider]),
+    TypeOrmModule.forFeature([
+      SpPerformanceLastWeek,
+      SpPerformanceAllTime,
+      MetricsDaily,
+      Deal,
+      Retrieval,
+      StorageProvider,
+    ]),
   ],
-  controllers: [ProvidersController, DailyMetricsController, FailedDealsController, NetworkStatsController],
-  providers: [MetricsSchedulerService, ProvidersService, DailyMetricsService, FailedDealsService, NetworkStatsService],
-  exports: [MetricsSchedulerService, ProvidersService, DailyMetricsService, FailedDealsService, NetworkStatsService],
+  controllers: [
+    ProvidersController,
+    DailyMetricsController,
+    FailedDealsController,
+    FailedRetrievalsController,
+    NetworkStatsController,
+  ],
+  providers: [
+    MetricsSchedulerService,
+    ProvidersService,
+    DailyMetricsService,
+    FailedDealsService,
+    FailedRetrievalsService,
+    NetworkStatsService,
+  ],
+  exports: [
+    MetricsSchedulerService,
+    ProvidersService,
+    DailyMetricsService,
+    FailedDealsService,
+    FailedRetrievalsService,
+    NetworkStatsService,
+  ],
 })
 export class MetricsModule {}
