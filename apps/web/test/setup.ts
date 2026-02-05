a26ac243c0866e24519514aded9d00ea2f0d1d04
silent-cipher <puspendramahariya@gmail.com>
2026-02-05 12:48:25 +0530

    test(web): add jest-dom matchers to test setup

--- apps/web/test/setup.ts
+++ apps/web/test/setup.ts
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 import { afterAll, afterEach, beforeAll } from "vitest";
 import { server } from "./mocks/server";
