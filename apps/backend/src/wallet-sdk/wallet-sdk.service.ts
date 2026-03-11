3ef47c8708a9f3a3e54eee0250c6e4ce5fac6f78
Julian Gruber <julian@juliangruber.com>
2026-03-11 09:14:00 +0100

    progress

--- apps/backend/src/wallet-sdk/wallet-sdk.service.ts
+++ apps/backend/src/wallet-sdk/wallet-sdk.service.ts
@@ -553,7 +553,7 @@ export class WalletSdkService implements OnModuleInit {
       const entities = Array.from(dedupedProviders.values()).map((info) =>
         this.spRepository.create({
           address: info.serviceProvider as Hex,
-          providerId: info.id,
+          providerId: Number(info.id),
           name: info.name,
           description: info.description,
           payee: info.payee,
