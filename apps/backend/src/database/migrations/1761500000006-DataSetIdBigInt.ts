2dad543c32e1d72685f9e0518815d030a8063656
Julian Gruber <julian@juliangruber.com>
2026-03-16 10:47:56 +0100

    fix sql

--- apps/backend/src/database/migrations/1761500000006-DataSetIdBigInt.ts
+++ apps/backend/src/database/migrations/1761500000006-DataSetIdBigInt.ts
@@ -4,10 +4,10 @@ export class DataSetIdBigInt1761500000006 implements MigrationInterface {
   name = "DataSetIdBigInt1761500000006";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE deals ALTER COLUMN data_set_id TYPE TEXT USING deals.data_set_id`);
+    await queryRunner.query(`ALTER TABLE deals ALTER COLUMN data_set_id TYPE TEXT`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE deals ALTER COLUMN data_set_id TYPE INTEGER USING deals.data_set_id`);
+    await queryRunner.query(`ALTER TABLE deals ALTER COLUMN data_set_id TYPE INTEGER USING deals.data_set_id::integer`);
   }
 }
